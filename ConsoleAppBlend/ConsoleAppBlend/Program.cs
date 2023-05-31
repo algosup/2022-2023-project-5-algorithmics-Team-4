f34c862639d7b8870b51da6cad22f3374d01b4f2
Salaheddine Namir <71770514+T3rryc@users.noreply.github.com>
2023-05-31 10:11:37 +0200

    Blending & Filling Code

---
+++ ConsoleAppBlend/ConsoleAppBlend/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleAppBlend
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Blender.Blending();
+          
+        }
+        
+    }
+
+}
