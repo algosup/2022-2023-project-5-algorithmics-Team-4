669735bfd63b7ffef831d8f3aaac07bf5d44339c
Salaheddine Namir <71770514+T3rryc@users.noreply.github.com>
2023-06-09 10:30:12 +0200

    Update Program.cs

--- ConsoleAppBlend/ConsoleAppBlend/Program.cs
+++ ConsoleAppBlend/ConsoleAppBlend/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 
 namespace ConsoleAppBlend
@@ -8,10 +9,32 @@ namespace ConsoleAppBlend
     {
         static void Main(string[] args)
         {
-            Blender.Blending();
-          
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            // MeasureExecutionTime(() => Blender.Blending());
+            MeasureExecutionTime(() => WineBlending.SetBlendingTanks(200, 200));
+            MeasureExecutionTime(() => WineBlending.SetWineTanks(100, 200, "A"));
+            MeasureExecutionTime(() => WineBlending.SetRatio(0.5, "A"));
+            MeasureExecutionTime(() => WineBlending.SetWineTanks(300, 300, "B"));
+            MeasureExecutionTime(() => WineBlending.SetWineTanks(500, 300, "B"));
+            MeasureExecutionTime(() => WineBlending.SetRatio(0.2, "B"));
+            MeasureExecutionTime(() => WineBlending.SetWineTanks(200, 300, "C"));
+            MeasureExecutionTime(() => WineBlending.SetRatio(0.3, "C"));
+            MeasureExecutionTime(() => WineBlending.Blend());
+            MeasureExecutionTime(() => WineBlending.Save());
+
+
         }
-        
+        static void MeasureExecutionTime(Action action)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action.Invoke();
+            stopwatch.Stop();
+            Console.WriteLine("Execution time: " + stopwatch.ElapsedMilliseconds + " ms");
+            Console.WriteLine();
+        }
+
     }
 
 }
